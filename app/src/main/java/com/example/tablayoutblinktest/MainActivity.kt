97a06cd70a3efd7f4e4d50e8a6b358d5dc5f87c2
minsuk.jang <minsuk.jang@influential.co.kr>
2022-10-17 16:37:15 +0900

    테스트 환경 구성

--- app/src/main/java/com/example/tablayoutblinktest/MainActivity.kt
+++ app/src/main/java/com/example/tablayoutblinktest/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
 import com.example.tablayoutblinktest.databinding.ActivityMainBinding
 import com.example.tablayoutblinktest.model.TabStatus
 import com.google.android.material.tabs.TabLayout
@@ -19,17 +20,71 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
 
         bind {
             lifecycleOwner = this@MainActivity
+            viewPager.adapter = CustomAdapter()
+            viewPager1.adapter = CustomAdapter()
         }
 
-        initViewPager2()
+        initTabLayout()
+        initTabLayout1()
     }
 
-    private fun initViewPager2() {
+    private fun initTabLayout1() {
         bind {
-            viewPager.adapter = CustomAdapter()
+            with(tabLayout1) {
+                tabNames.forEachIndexed { index, s ->
+                    addTab(newTab().apply {
+                        customView = CustomTabView(this@MainActivity).apply {
+                            status = TabStatus(s, index == 0)
+                        }
+                    })
+                }
 
-            with(tabLayout){
-                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+                        (tab?.customView as? CustomTabView)?.bind {
+                            status = status.copy(
+                                status = true
+                            )
+                        }
+
+                        binding.viewPager.setCurrentItem(tab?.position ?: 0, true)
+                    }
+
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+                        (tab?.customView as? CustomTabView)?.bind {
+                            status = status.copy(
+                                status = false
+                            )
+                        }
+                    }
+
+                    override fun onTabReselected(tab: TabLayout.Tab?) {
+                    }
+                })
+            }
+        }
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+                binding.tabLayout1.setScrollPosition(position, positionOffset, true, true)
+
+                if (positionOffset == 0.0f) {
+                    val tab = binding.tabLayout1.getTabAt(position)
+                    binding.tabLayout1.selectTab(tab)
+                }
+            }
+
+            override fun onPageSelected(position: Int) {}
+
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
+    }
+
+    private fun initTabLayout() {
+        bind {
+
+            with(tabLayout) {
+                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
                     override fun onTabSelected(tab: TabLayout.Tab?) {
                         (tab?.customView as? CustomTabView)?.bind {
                             status = status.copy(
@@ -51,13 +106,11 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
             }
         }
 
-        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+        TabLayoutMediator(binding.tabLayout, binding.viewPager1) { tab, position ->
             tab.customView = CustomTabView(this).apply {
                 status = TabStatus("테스트 $position", position == 0)
             }
         }.attach()
-
-
     }
 
     inner class CustomAdapter : FragmentStateAdapter(this) {
