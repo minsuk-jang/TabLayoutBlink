f54869d01e5da204bf1e48a518a78148679c95eb
Minsuk <minsuk.jang@influential.co.kr>
2023-02-18 16:30:21 +0900

    테스트 환경

--- app/src/main/java/com/example/tablayoutblinktest/GlobalApplication.kt
+++ app/src/main/java/com/example/tablayoutblinktest/GlobalApplication.kt
@@ -1,8 +1,14 @@
 package com.example.tablayoutblinktest
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+    }
 }
--- app/src/main/java/com/example/tablayoutblinktest/MainActivity.kt
+++ app/src/main/java/com/example/tablayoutblinktest/MainActivity.kt
@@ -31,64 +31,6 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
         initTabLayout1()
     }
 
-    private fun initTabLayout1() {
-        bind {
-            with(tabLayout1) {
-                tabNames.forEachIndexed { index, s ->
-                    addTab(newTab().apply {
-                        customView = CustomTabView(this@MainActivity).apply {
-                            status = TabStatus(s, index == 0)
-                        }
-                    })
-                }
-
-                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
-                    override fun onTabSelected(tab: TabLayout.Tab?) {
-                        (tab?.customView as? CustomTabView)?.bind {
-                            status = status.copy(
-                                status = true
-                            )
-                        }
-
-                        binding.viewPager.setCurrentItem(tab?.position ?: 0, true)
-                    }
-
-                    override fun onTabUnselected(tab: TabLayout.Tab?) {
-                        (tab?.customView as? CustomTabView)?.bind {
-                            status = status.copy(
-                                status = false
-                            )
-                        }
-                    }
-
-                    override fun onTabReselected(tab: TabLayout.Tab?) {
-                    }
-                })
-            }
-        }
-
-        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
-            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
-                Log.e(
-                    TAG,
-                    "Offset: $positionOffset\n"
-                )
-                binding.tabLayout1.setScrollPosition(position, positionOffset, true, true)
-
-                if (positionOffset == 0.0f) {
-                    val tab = binding.tabLayout1.getTabAt(position)
-                    binding.tabLayout1.selectTab(tab)
-                }
-            }
-
-            override fun onPageSelected(position: Int) {
-                Log.e(TAG, "onPageSelected: ")
-            }
-
-            override fun onPageScrollStateChanged(state: Int) {}
-        })
-    }
-
     private fun initTabLayout() {
         bind {
 
@@ -120,6 +62,67 @@ class MainActivity : BaseActivity<ActivityMainBinding>(R.layout.activity_main) {
                 status = TabStatus("테스트 $position", position == 0)
             }
         }.attach()
+    }
+
+    private fun initTabLayout1() {
+        bind {
+            with(tabLayout1) {
+                tabNames.forEachIndexed { index, s ->
+                    addTab(newTab().apply {
+                        customView = CustomTabView(this@MainActivity).apply {
+                            status = TabStatus(s, index == 0)
+                        }
+                    })
+                }
+
+                addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+                    override fun onTabSelected(tab: TabLayout.Tab?) {
+                        Log.e(TAG, "onTabSelected: ${tab?.position}")
+                        (tab?.customView as? CustomTabView)?.bind {
+                            status = status.copy(
+                                status = true
+                            )
+                        }
+
+                        binding.viewPager.setCurrentItem(tab?.position ?: 0, true)
+                    }
+
+                    override fun onTabUnselected(tab: TabLayout.Tab?) {
+                        Log.e(TAG, "onTabUnselected: ${tab?.position}", )
+                        (tab?.customView as? CustomTabView)?.bind {
+                            status = status.copy(
+                                status = false
+                            )
+                        }
+                    }
+
+                    override fun onTabReselected(tab: TabLayout.Tab?) {
+                    }
+                })
+            }
+        }
+
+        binding.viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageScrolled(
+                position: Int,
+                positionOffset: Float,
+                positionOffsetPixels: Int
+            ) {
+                binding.tabLayout1.setScrollPosition(position, positionOffset, true, true)
+                if (positionOffset == 0.0f) {
+                    val tab = binding.tabLayout1.getTabAt(position)
+                    binding.tabLayout1.selectTab(tab)
+                }
+            }
+
+            override fun onPageSelected(position: Int) {
+                /*Log.e(TAG, "onPageSelected: $position")
+                val tab = binding.tabLayout1.getTabAt(position)
+                binding.tabLayout1.selectTab(tab,false)*/
+            }
+
+            override fun onPageScrollStateChanged(state: Int) {}
+        })
     }
 
     inner class CustomAdapter : FragmentStateAdapter(this) {
